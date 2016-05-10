4204cbe9a34c5a5f2dd040725d29689067322126
Jon Barker <j.p.barker@sheffield.ac.uk>
2016-05-10 19:34:36 +0100

    fixed setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,4 @@
 copy OCR.v3.ipynb "U:\ManWin\My Documents\IPython Notebooks"
-xcopy python_ocr "U:\ManWin\My Documents\IPython Notebooks" \E
+xcopy python_ocr "U:\ManWin\My Documents\IPython Notebooks\python_ocr\" \E \T
+
 
